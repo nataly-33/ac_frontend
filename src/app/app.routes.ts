b487275091cd715f72c80ddf79b5dabaf793d869
nataly-33 <vanessamartinez1@upb.edu>
2026-05-30 04:09:52 -0400

    chore:se actualizo variables de entorno y librerias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/landing/landing').then(m => m.Landing) },
+  { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard) },
+  { path: 'municipio/:id', loadComponent: () => import('./pages/municipio-detalle/municipio-detalle').then(m => m.MunicipioDetalle) },
+  { path: '**', redirectTo: '' }
+];
